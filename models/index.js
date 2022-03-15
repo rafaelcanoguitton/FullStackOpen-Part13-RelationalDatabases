89163059568c0bb7ad5286f0a4b9167b531f42a2
Rafael Isaac Cano Guitton <rafael.cano@ucsp.edu.pe>
2022-03-14 20:47:26 -0500

    13.19 done.

--- models/index.js
+++ models/index.js
@@ -1,12 +1,15 @@
 const Blog = require("./blog");
 const User = require("./user");
-
+const ReadingList = require("./readinglist");
 User.hasMany(Blog);
 Blog.belongsTo(User);
 //Blog.sync({ alter: true });
 //User.sync({ alter: true });
+User.belongsToMany(Blog, { through: ReadingList });
+Blog.belongsToMany(User, { through: ReadingList });
 
 module.exports = {
   Blog,
   User,
+  ReadingList,
 };
